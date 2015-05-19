d5c5e020f18d3c6bac0d0407aa28789b201f4d9c
Matt Chadburn <matt.chadburn@ft.com>
2015-05-19 17:25:56 +0100

    Revert "100%"
    This reverts commit 5976c8f77f37fe504c05db4853f7be4486ea0d53.

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ var sqsUrl = process.env.SQS_URL;
 					var parser		= new UAParser();
 					parser.setUA(h['user-agent']);
 
-					if (Math.random() < 2) { 
+					if (Math.random() < 0.01) { 
 						pusher.trigger('test_channel', 'my_event', {
 							"message": { 
 								referer: h.referer,
