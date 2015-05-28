b33962a0aaee4b1ca56a7c197067c56b75113902
Matt Chadburn <matt.chadburn@ft.com>
2015-05-28 09:10:37 +0100

    Use the correct receipt id (for delete)

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ var sqsUrlIngest = process.env.SQS_INGEST;
 							
 							sqs.deleteMessage({
 								QueueUrl: sqsUrlIngest,
-								ReceiptHandle: receiptId 	
+								ReceiptHandle: meta.receiptHandle
 							}, function(err, data) {
 								if (err) console.log(err, err.stack); // an error occurred
 								else     console.log('DELETED', data);           // successful response		
