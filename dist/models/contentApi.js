c3f02f08e9619d6ae2de4aecd606501b6a518057
Matt Chadburn <matt.chadburn@ft.com>
2015-06-10 21:38:03 +0100

    Don't reject CAPI v2 404s

--- dist/models/contentApi.js
+++ dist/models/contentApi.js
@@ -2,6 +2,7 @@
 
 var url = require('url');
 var fetch = require('node-fetch');
+var statsd = require('../lib/statsd');
 
 var isArticle = /([a-f0-9-]{36})/;
 
@@ -23,11 +24,14 @@ module.exports = function (referrer) {
 		// FIXME - extract uuid from r.pathname
 		console.log('models/content-api', 'fetching n', article[0]);
 
+		statsd.increment('ingest.consumer.models.content-api.fetch.request', 1);
+
 		fetch('http://api.ft.com/content/' + article[0], {
 			timeout: 2000,
 			headers: { 'x-api-key': process.env.CAPI_API_KEY }
 		}).then(function (res) {
 			console.log('models/content-api', article[0], res.status);
+			statsd.increment('ingest.consumer.models.content-api.fetch.response.' + res.status, 1);
 			return res.json();
 		}).then(function (content) {
 			console.log('models/content-api', article[0], content.title);
@@ -39,7 +43,8 @@ module.exports = function (referrer) {
 			});
 		})['catch'](function (err) {
 			console.log('models/content-api', article[0], 'error', err);
-			reject(err);
+			statsd.increment('ingest.consumer.models.content-api.error', 1);
+			resolve({}); // FIXME - could/should be a reject
 		});
 	});
 };
