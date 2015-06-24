b063399bbffe71c34c366a49093b526d52a42363
Matt Chadburn <matt.chadburn@ft.com>
2015-06-24 09:31:58 +0100

    move 'time' to the EventModel

--- server/transforms/time.js
+++ server/transforms/time.js
@@ -2,12 +2,15 @@
 var moment = require('moment');
 
 module.exports = function (event) {
-	event.egest.time = {
+
+	var time = {
 		weekday: !/^0|6$/.test(new Date().getDay()),
 		day: new Date().toJSON().slice(0, 10) + 'T00:00:00Z',
 		hour: new Date().toJSON().slice(0, 14) + '00:00Z',
 		now: new Date().toJSON(),
 		week: moment().format('YYYYw')
 	}
+
+	event.annotate('time', time);
 	return event;
 }
--- tests/server/transforms/timeSpec.js
+++ tests/server/transforms/timeSpec.js
@@ -7,50 +7,56 @@ var sinon	= require('sinon');
 var fs		= require('fs');
 
 var time	= require('../../../dist/transforms').time;
-var e = {
-	ingest: fs.readFileSync('./tests/server/fixtures/ingest', { encoding: 'utf8' }),
-	egest: {}
-}
+var EventModel	= require('../../../dist/models').EventModel;
+
+var rawSqs = JSON.parse(fs.readFileSync('./tests/server/fixtures/ingest', { encoding: 'utf8' }));
+var e;
 
 describe('Time', function () {
-
+	
 	beforeEach(() => {
-		e.egest = {};
-	})
+		e = new EventModel(rawSqs);
+	});
 
 	it('Flag if the event happened on a weekend', done => {
-		var t = sinon.useFakeTimers(new Date('2015-3-1').getTime());
-		expect(time(e).egest.time.weekday).to.equal(false);
+		var ft = sinon.useFakeTimers(new Date('2015-3-1').getTime());
+		var t = time(e);
+		expect(e.annotations().time.weekday).to.equal(false);
 		done();
 	});
 
 	it('Flag if the event happened on a weekday', done => {
-		var t = sinon.useFakeTimers(new Date('2015-3-2').getTime());
-		expect(time(e).egest.time.weekday).to.equal(true);
+		var ft = sinon.useFakeTimers(new Date('2015-3-2').getTime());
+		var t = time(e);
+		expect(e.annotations().time.weekday).to.equal(true);
 		done()
 	});
 
 	it('Round the time of the event to the ISO day', done => {
-		var t = sinon.useFakeTimers(new Date('2015-3-2').getTime());
-		expect(time(e).egest.time.day).to.equal('2015-03-02T00:00:00Z');
+		var ft = sinon.useFakeTimers(new Date('2015-3-2').getTime());
+		var t = time(e);
+		expect(e.annotations().time.day).to.equal('2015-03-02T00:00:00Z');
 		done()
 	});
 	
 	it('Round the week of the year the event happened', done => {
-		var t = sinon.useFakeTimers(new Date('Mon, 15 Jun 2015 20:12:01 UTC').getTime());
-		expect(time(e).egest.time.week).to.equal('201525'); // 25th week of 2015
+		var ft = sinon.useFakeTimers(new Date('Mon, 15 Jun 2015 20:12:01 UTC').getTime());
+		var t = time(e);
+		expect(e.annotations().time.week).to.equal('201525'); // 25th week of 2015
 		done()
 	});
 	
 	it('Round the time of the event to the ISO hour', done => {
-		var t = sinon.useFakeTimers(new Date('Mon, 15 Jun 2015 20:12:01 UTC').getTime());
-		expect(time(e).egest.time.hour).to.equal('2015-06-15T20:00:00Z');
+		var ft = sinon.useFakeTimers(new Date('Mon, 15 Jun 2015 20:12:01 UTC').getTime());
+		var t = time(e);
+		expect(e.annotations().time.hour).to.equal('2015-06-15T20:00:00Z');
 		done()
 	});
 	
 	it('Round the time of the event being logged', done => {
-		var t = sinon.useFakeTimers(new Date('Mon, 15 Jun 2015 20:12:01 UTC').getTime());
-		expect(time(e).egest.time.now).to.equal('2015-06-15T20:12:01.000Z');
+		var ft = sinon.useFakeTimers(new Date('Mon, 15 Jun 2015 20:12:01 UTC').getTime());
+		var t = time(e);
+		expect(e.annotations().time.now).to.equal('2015-06-15T20:12:01.000Z');
 		done()
 	});
 
