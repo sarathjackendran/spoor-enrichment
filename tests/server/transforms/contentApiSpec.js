de804d99084e20b0b4e7d1be3a74c9baf5d1dae7
Matt Chadburn <matt.chadburn@ft.com>
2015-06-29 19:30:23 +0100

    Pluck the UUID from the message body, fallback to referrer

--- tests/server/transforms/contentApiSpec.js
+++ tests/server/transforms/contentApiSpec.js
@@ -11,12 +11,13 @@ var contentApi = require('../../../dist/transforms').contentApi;
 var EventModel = require('../../../dist/models').EventModel;
 
 var rawSqs = JSON.parse(fs.readFileSync('./tests/server/fixtures/ingest', { encoding: 'utf8' }));
-var e;
+var rawSqs__no_referer = JSON.parse(fs.readFileSync('./tests/server/fixtures/ingest--no-referer', { encoding: 'utf8' }));
+var rawSqs__content_id = JSON.parse(fs.readFileSync('./tests/server/fixtures/ingest--content-id', { encoding: 'utf8' }));
+var capi__response = fs.readFileSync('./tests/server/fixtures/capi__response', { encoding: 'utf8' }); 
 
-describe('Session API', function() {
+describe('Content API', function() {
 
 	beforeEach(() => {
-		e = new EventModel(rawSqs);
 		this.mitm = Mitm();
 	})
 	
@@ -24,7 +25,8 @@ describe('Session API', function() {
 		this.mitm.disable();
 	})
 	
-	it('Validate a cookie against the Session API', done => {
+	it('Enrich an event with information from the Content API', done => {
+		var e = new EventModel(rawSqs);
 		this.mitm.on("request", function(req, res) {
 			expect(req.headers.host).to.equal('api.ft.com');
 			expect(req.url).to.equal('/content/06d28cd0-055b-11e5-bb7d-00144feabdc0');
@@ -33,5 +35,55 @@ describe('Session API', function() {
 		})
 		contentApi(e);
 	});
+	
+	it('Enrich an event with uuid, headline and published date', done => {
+		var e = new EventModel(rawSqs);
+		this.mitm.on("request", function(req, res) {
+			res.statusCode = 200;
+			res.end(capi__response);
+		})
+		contentApi(e)
+			.then(function (content) {
+				expect(content.uuid).to.equal('06d28cd0-055b-11e5-bb7d-00144feabdc0');
+				expect(content.title).to.equal('Osborne bids to bring voters onside with pre-poll Budget');
+				expect(content.publishedDate).to.equal('2015-03-17T19:43:46.000Z');
+				done();
+			});
+	});
+	
+	it('Pluck the reference the the content UUID from message body if it exists', done => {
+		var e = new EventModel(rawSqs__content_id);
+		this.mitm.on("request", function(req, res) {
+			expect(req.url).to.equal('/content/0f7464b4-3f4d-11e4-984b-00144feabdc0');
+			done();
+		});
+		contentApi(e)
+			.catch(err => console.log(err));
+	});
 
+	
+	it('Do not enrich articles with no referer or UUID in the message body', done => {
+		var e = new EventModel(rawSqs__no_referer);
+		contentApi(e)
+			.then(function (res) {
+				expect(res).to.deep.equal({});
+				done();
+			});
+	});
+		
+
+	it('Error from Content API', done => {
+		var e = new EventModel(rawSqs);
+		this.mitm.on("request", function(req, res) {
+			res.statusCode = 503;
+			res.end('{}');
+		})	
+		contentApi(e)
+			.then(function (res) {
+				expect(res).to.deep.equal({});
+				done();
+			})
+			.catch(err => console.log(err));
+	});
+	
 });
