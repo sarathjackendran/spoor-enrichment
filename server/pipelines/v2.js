0441e2345c5582a306d2e04953aecf6068075d8e
Matt Chadburn <matt.chadburn@ft.com>
2015-07-02 14:01:51 +0100

    Instrument pipeline

--- server/pipelines/v2.js
+++ server/pipelines/v2.js
@@ -5,6 +5,7 @@ var sinks			= require('../sinks');
 var filters			= require('../filters');
 var transforms		= require('../transforms');
 var EventModel		= require('../models').EventModel;
+var metrics			= require('next-metrics')
 
 var EventEmitter	= require('events').EventEmitter;
 
@@ -17,6 +18,10 @@ require('es6-promise').polyfill();
 
 module.exports = stream => {
 	stream
+		.pipe(es.map((data, next) => {
+			metrics.count('pipeline.v2.in', 1);
+			next(null, data);
+		}))
 		.pipe(es.parse())
 		.pipe(es.map((data, next) => {
 			var event = new EventModel(data);
@@ -57,11 +62,13 @@ module.exports = stream => {
 				.catch(err => {
 					console.log('error', err);
 					next(err, event);	// TODO annotate an 'error' flag
+					metrics.count('pipeline.v2.error', 1);
 				});
 		}))
 		.pipe(es.stringify())
 		.pipe(es.map((event, next) => {
 			emitter.emit('enriched', event);
+			metrics.count('pipeline.v2.out', 1);
 			next(null, event);
 		}))
 		.pipe(process.stdout)
