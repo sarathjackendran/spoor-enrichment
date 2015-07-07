f5416f3e65367a880276e03d706fe2a0535e63be
George Crawford <george.crawford@ft.com>
2015-07-07 21:02:03 +0100

    Pluck UA from message body if set, falling back to header

--- server/transforms/userAgent.js
+++ server/transforms/userAgent.js
@@ -3,15 +3,16 @@ var UAParser	= require('ua-parser-js');
 var metrics		= require('next-metrics');
 
 module.exports = function (event) {
-	
+
 	metrics.count('pipeline.transforms.userAgent.count', 1);
-	
+
 	var headers = event.headers();
-	
-	if (!headers['user-agent']) return event;
-	
+
+	var ua = event.pluck('device.user_agent') || headers['user-agent'];
+	if (!ua) return event;
+
 	var parser = new UAParser();
-	parser.setUA(headers['user-agent']);
+	parser.setUA(ua);
 	event.annotate('ua', parser.getResult());
 	return event;
 }
