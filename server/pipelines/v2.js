ac3d1048fbcf3679498133a42b4aa4fb4f6e742e
Matt Chadburn <matt.chadburn@ft.com>
2015-07-10 22:28:19 +0100

    track 100ms exceeds

--- server/pipelines/v2.js
+++ server/pipelines/v2.js
@@ -104,6 +104,10 @@ module.exports = stream => {
 			var end = process.hrtime(start);
 			console.info("execution time (hr): %ds %dms", end[0], end[1]/1000000, end);
 			
+			if (end[1]/1000000 > 100) {
+				metrics.count('pipeline.v2.execution_time.exceeded_100ms', 1);
+			}
+			
 			if (end[1]/1000000 > 500) {
 				metrics.count('pipeline.v2.execution_time.exceeded_500ms', 1);
 			}
