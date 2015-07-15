f1e84e26f84de979765b6e171907ff938fb51178
Matt Chadburn <matt.chadburn@ft.com>
2015-07-15 21:39:42 +0100

    Time tests

--- tests/server/transforms/timeSpec.js
+++ tests/server/transforms/timeSpec.js
@@ -23,21 +23,21 @@ describe('Time', function () {
 	it('Flag if the event happened on a weekend', done => {
 		var ft = sinon.useFakeTimers(new Date('2015-3-1').getTime());
 		var t = time(e);
-		expect(e.annotations().time.weekday).to.equal(false);
+		expect(e.annotations().time.isWeekday).to.equal(false);
 		done();
 	});
 
 	it('Flag if the event happened on a weekday', done => {
 		var ft = sinon.useFakeTimers(new Date('2015-3-2').getTime());
 		var t = time(e);
-		expect(e.annotations().time.weekday).to.equal(true);
+		expect(e.annotations().time.isWeekday).to.equal(true);
 		done()
 	});
 
 	it('Round the time of the event to the ISO day', done => {
 		var ft = sinon.useFakeTimers(new Date('2015-3-2').getTime());
 		var t = time(e);
-		expect(e.annotations().time.day).to.equal('2015-03-02T00:00:00Z');
+		expect(e.annotations().time.iso.day).to.equal('2015-03-02T00:00:00Z');
 		done()
 	});
 	
@@ -51,14 +51,14 @@ describe('Time', function () {
 	it('Round the time of the event to the ISO hour', done => {
 		var ft = sinon.useFakeTimers(new Date('Mon, 15 Jun 2015 20:12:01 UTC').getTime());
 		var t = time(e);
-		expect(e.annotations().time.hour).to.equal('2015-06-15T20:00:00Z');
+		expect(e.annotations().time.iso.hour).to.equal('2015-06-15T20:00:00Z');
 		done()
 	});
 	
 	it('Round the time of the event being logged', done => {
 		var ft = sinon.useFakeTimers(new Date('Mon, 15 Jun 2015 20:12:01 UTC').getTime());
 		var t = time(e);
-		expect(e.annotations().time.now).to.equal('2015-06-15T20:12:01.000Z');
+		expect(e.annotations().time.iso.now).to.equal('2015-06-15T20:12:01.000Z');
 		done()
 	});
 	
@@ -66,7 +66,7 @@ describe('Time', function () {
 		var ft = sinon.useFakeTimers(new Date('Mon, 15 Jun 2015 20:12:01 UTC').getTime());
 		var offset = new EventModel(rawSqs__time_offset);
 		time(offset);
-		expect(offset.annotations().time.now).to.equal('2015-06-16T16:22:20.795Z'); // 'time received' minus 10000ms
+		expect(offset.annotations().time.iso.now).to.equal('2015-06-16T16:22:20.795Z'); // 'time received' minus 10000ms
 		expect(offset.annotations().time.offset).to.equal(100000);
 		done();
 	});
@@ -74,8 +74,20 @@ describe('Time', function () {
 	it('Use the time the event was received where specified', done => {
 		var offset = new EventModel(rawSqs);
 		time(offset);
-		expect(offset.annotations().time.now).to.equal('2015-06-16T16:24:00.795Z');
+		expect(offset.annotations().time.iso.now).to.equal('2015-06-16T16:24:00.795Z');
 		done();
 	});
-
+	
+	it('Generate various time tokens', done => {
+		var ft = sinon.useFakeTimers(new Date('Mon, 15 Jun 2015 20:12:01 UTC').getTime());
+		var t = time(e);
+		expect(e.annotations().time.dayOfWeek).to.equal(1);
+		expect(e.annotations().time.dayInWords).to.equal('Monday');
+		expect(e.annotations().time.dayOfYear).to.equal(166);
+		expect(e.annotations().time.date).to.equal(15);
+		expect(e.annotations().time.month).to.equal(6);
+		expect(e.annotations().time.monthInWords).to.equal('June');
+		expect(e.annotations().time.year).to.equal(2015);
+		done()
+	});
 });
